bb9026d2b3cc67cc535d6c6b25ec50cb8d808153
Vincent GICQUEL <37332811+vgicquel@users.noreply.github.com>
2018-04-03 10:22:29 +0200

    Create blog.js

---
+++ inklusif/blog.js
@@ -0,0 +1,13 @@
+////
+// @author Vincent GICQUEL LE GROUPE NOVA
+// @name Blog Customization
+// @version 0.1
+// @date April, 2018
+//
+if(typeof(dojo) != "undefined") {
+    dojo.place(
+        "<link rel=\"stylesheet\" type=\"text/css\" href=\"/files/customizer/novaliance-samples/blog.css?repoName=novaliance-samples\"></link>",
+        dojo.doc.head,
+        "last"
+    );
+}
