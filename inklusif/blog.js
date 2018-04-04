8b2f868d47653f218c4e41a814979af409c3f75c
Vincent GICQUEL <37332811+vgicquel@users.noreply.github.com>
2018-04-04 11:16:31 +0200

    Update blog.js

--- inklusif/blog.js
+++ inklusif/blog.js
@@ -4,15 +4,28 @@
 // @version 0.1
 // @date April, 2018
 //
-if(typeof(dojo) != "undefined") {
-    dojo.place(
-        "<link rel=\"stylesheet\" type=\"text/css\" href=\"/files/customizer/novaliance-samples/blog.css?repoName=novaliance-samples\"></link>",
-        dojo.doc.head,
-        "last"
-    );
-    try {
-        dojo.place("<hr>", dojo.doc.tbody,"last");
-    } catch (e) {
+
+if (typeof (dojo) != "undefined") {
+    require(["dojo/domReady!"], function () {
+        try {
+            dojo.place(
+               "<link rel=\"stylesheet\" type=\"text/css\" href=\"/files/customizer/novaliance-samples/blog.css?repoName=novaliance-samples\"></link>",
+				dojo.doc.head,
+				"last"
+            );
+        } catch (e) {
             alert('exception occurred : ' + e);
-    }
+        }
+		
+		try {
+            dojo.place(
+               "<hr>",
+				dojo.doc.tbody,
+				"last"
+            );
+        } catch (e) {
+            alert('exception occurred : ' + e);
+        }
+		
+    });
 }
