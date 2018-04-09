68081eeac6e31848d786b026be3fbf99ab8cebcb
Vincent GICQUEL <37332811+vgicquel@users.noreply.github.com>
2018-04-09 11:06:42 +0200

    Update blog.js

--- inklusif/blog.js
+++ inklusif/blog.js
@@ -25,7 +25,7 @@ if (typeof (dojo) != "undefined") {
 	    
 	try {
 		console.debug(dojo.query(".vcard"));
-		dojo.place("<hr class='hr-red'>", dojo.query(".vcard"),"last");	
+		dojo.place("<hr class='hr-red'>", dojo.query(".vcard")[0],"last");	
         } catch (e) {
             /*alert('exception occurred : ' + e);*/
 		console.debug('exception occurred : ' + e);
