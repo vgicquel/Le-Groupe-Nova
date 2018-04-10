a3bb87e49f894af408e53e2f65f8f5dbcd8891ab
Vincent GICQUEL <37332811+vgicquel@users.noreply.github.com>
2018-04-10 12:24:38 +0200

    Update blog.js

--- inklusif/blog.js
+++ inklusif/blog.js
@@ -9,10 +9,10 @@ if (typeof (dojo) != "undefined") {
     require(["dojo/domReady!"], function () {
 	/* Ajout d'une ligne horizontale après chaue entrée de blog */    
 	try {
-		console.debug(dojo.query(".vcard"));
+		console.debug(dojo.query(".entryContentContainerTD"));
 		var n;
-		for (n=2; n <= dojo.query(".vcard").length; n=n+2) {
-			dojo.place("<hr class='hr-color'>", dojo.query(".vcard")[n],"last");
+		for (n=0; n <= dojo.query(".entryContentContainerTD").length; n++) {
+			dojo.place("<hr class='hr-color'>", dojo.query(".entryContentContainerTD")[n],"last");
 		}
         } catch (e) {
             /*alert('exception occurred : ' + e);*/
