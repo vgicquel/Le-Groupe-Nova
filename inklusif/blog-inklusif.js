6da7ecad2cd4b59bbf39560d85e722ef8d6e4544
Vincent GICQUEL <37332811+vgicquel@users.noreply.github.com>
2018-06-22 12:02:41 +0200

    Update blog-inklusif.js

--- inklusif/blog-inklusif.js
+++ inklusif/blog-inklusif.js
@@ -2,10 +2,10 @@ let BlogJS = new __cBill_waitForDojo('BlogJS');
 BlogJS.do(function () { 
 	
 		/* Recherche de la balise */    
-		console.debug(dojo.query(".lotusinlinelist"));
+		console.debug(dojo.query(".inlinelist"));
 		var n;
-		for (n=0; n <= dojo.query(".lotusinlinelist").length; n++) {
-		  dojo.place("<div class='lotusActionBar lotusBtnContainer aria_toolbar' role='toolbar' style='float:none' aria-label='Actions de blogue'>", dojo.query(".lotusinlinelist")[n],"replace");
+		for (n=0; n <= dojo.query(".inlinelist").length; n++) {
+		  dojo.place("<div class='lotusActionBar lotusBtnContainer aria_toolbar' role='toolbar' style='float:none' aria-label='Actions de blogue'>", dojo.query(".inlinelist")[n],"replace");
 		}
 
 		
