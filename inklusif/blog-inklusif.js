e34a61d876b9f2938d9dcec457525d24bd78f946
Vincent GICQUEL <37332811+vgicquel@users.noreply.github.com>
2018-06-22 12:59:06 +0200

    Update blog-inklusif.js

--- inklusif/blog-inklusif.js
+++ inklusif/blog-inklusif.js
@@ -5,7 +5,19 @@ BlogJS.do(function () {
 		console.debug(dojo.query(".inlinelist"));
 		var n;
 		for (n=0; n <= dojo.query(".inlinelist").length; n++) {
-		  dojo.place("<div class='lotusActionBar lotusBtnContainer aria_toolbar' role='toolbar' style='float:none' aria-label='Actions de blogue'>", dojo.query(".inlinelist")[n],"replace");
+		  dojo.place("
+			     <div class='lotusActionBar lotusBtnContainer aria_toolbar' role='toolbar' style='float:none' aria-label='Actions de blogue'>
+			     	<span class='lotusBtn lotusLeft'>
+					<a href='https://apps.na.collabserv.com/blogs/a080f064-36ea-4105-8121-7a6a78acef94/entry/Inklusif_septieme_partie?lang=fr_fr' title='Entrée précédente' hastooltip='dijit_Tooltip_3711'>Entrée précédente</a>
+				</span>
+				<span class='lotusBtn lotusLeft'>
+					<a href='/blogs/a080f064-36ea-4105-8121-7a6a78acef94?maxresults=25&amp;lang=fr_fr'>Page principale</a>
+        			</span>
+        			<span class='lotusBtn lotusLeft'>
+	    				<a>Entrée suivante</a>
+				</span>
+				</div>
+			     ", dojo.query(".inlinelist")[n],"replace");
 		}
 
 		
