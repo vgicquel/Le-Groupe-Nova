9a227317a2d407db0aca1a4adbe2b729714f14c0
Vincent GICQUEL <37332811+vgicquel@users.noreply.github.com>
2018-06-26 10:43:49 +0200

    Update blog-inklusif.js

--- inklusif/blog-inklusif.js
+++ inklusif/blog-inklusif.js
@@ -1,32 +1,12 @@
 let BlogJS = new __cBill_waitForDojo('BlogJS'); 
 BlogJS.do(function () { 
 	
-		/* Suppression de la ligne noire */
+		/* Recherche de la balise */ 
 		console.debug(dojo.query("div[style='border-top: 2px solid black;height:45px;']"));
-		/*dojo.place("<div style='border-top: 0px solid black;height:45px;'>",dojo.query("div[style='border-top: 2px solid black;height:45px;']")[0],"replace");*/
-	
-		/* Test de remplacement	de style */
+
+		/* Suppression de la ligne noire */
 		dojo.query("div[style='border-top: 2px solid black;height:45px;']").style("border-top","0px solid black")
 	
-		/* Recherche de la balise */    
-		console.debug(dojo.query(".inlinelist"));
-		var n;
-	
-		/* Remplacement */
-		/*for (n=0; n <= dojo.query(".inlinelist").length; n++) {
-		  dojo.place("<div class='lotusActionBar lotusBtnContainer aria_toolbar' role='toolbar' style='float:none' aria-label='Actions de blogue'>" + 
-			     "<span class='lotusBtn lotusLeft'>" +
-			     "<a href='https://apps.na.collabserv.com/blogs/a080f064-36ea-4105-8121-7a6a78acef94/entry/Inklusif_septieme_partie?lang=fr_fr' title='Entrée précédente' hastooltip='dijit_Tooltip_3711'>Entrée précédente</a>" +
-			     "</span>" +
-			     "<span class='lotusBtn lotusLeft'>" +
-			     "<a href='/blogs/a080f064-36ea-4105-8121-7a6a78acef94?maxresults=25&amp;lang=fr_fr'>Page principale</a>" +
-			     "</span>" +
-			     "<span class='lotusBtn lotusLeft'>" +
-			     "<a>Entrée suivante</a>" +
-			     "</span>" +
-			     "</div>", dojo.query(".inlinelist")[n],"replace");
-		}*/
-
 	}
 	  
 )
