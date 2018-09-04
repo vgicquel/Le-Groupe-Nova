838e273661e8d1ae293eb6cb17d3278260fd86fd
Vincent GICQUEL <37332811+vgicquel@users.noreply.github.com>
2018-09-04 10:46:28 +0200

    Update test.js

--- test_agefiph/test.js
+++ test_agefiph/test.js
@@ -1,1 +1,12 @@
-
+////
+// @author Vincent GICQUEL LE GROUPE NOVA
+// @name Test Customization JS
+// @version 0.1
+// @date September, 2018
+//
+let BlogJS = new __cBill_waitForDojo('BlogJS'); 
+BlogJS.do(function () { 
+  
+  	}
+	  
+)
