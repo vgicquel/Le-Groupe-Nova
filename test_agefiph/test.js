ce53b02016c10f80bdec71f864b8e8bc57e6c284
Vincent GICQUEL <37332811+vgicquel@users.noreply.github.com>
2018-09-10 16:09:16 +0200

    Update test.js

--- test_agefiph/test.js
+++ test_agefiph/test.js
@@ -9,21 +9,37 @@ TestJS.do(function () {
   
 		/* Recherche de la balise */    
 		console.debug(dojo.query("ul[class='inlinelist lotusInlinelist']"));
-	
+		
 		var list = document.getElementsByClassName("inlinelist lotusInlinelist")[0].getElementsByTagName("LI");
-
+			for (i=0; i < list.length; i++) {
+			//alert(list[i].innerHTML);
+		}
+		
+		if (list[0].innerHTML.includes("</a>")) {
+				alert(list[0].innerHTML);
+		} else {
+			list[0].innerHTML = "<a>" + list[0].innerHTML + "</a>";
+		}
+		
+		if (list[2].innerHTML.includes("</a>")) {
+				alert(list[2].innerHTML);
+		} else {
+			list[2].innerHTML = "<a>" + list[2].innerHTML + "</a>";
+		}
+		
 		for (n=0; n <= dojo.query("ul[class='inlinelist lotusInlinelist']").length; n++) {
 			  dojo.place("<div class='lotusActionBar lotusBtnContainer aria_toolbar' role='toolbar' style='float:none' aria-label='Actions de blogue'>" + 
-				 "<span class='lotusBtn lotusLeft'>" +
-				 list[0].innerHTML + 
-				 "</span>" +
-				 "<span class='lotusBtn lotusLeft'>" +
-				 list[1].innerHTML +
-				 "</span>" +
-				 "<span class='lotusBtn lotusLeft'>" +
-				 list[2].innerHTML +
-				 "</span>" +
-				 "</div>", dojo.query(".inlinelist")[n],"replace");
+					 "<span class='lotusBtn lotusLeft'>" +
+					 list[0].innerHTML + 
+					 "</span>" +
+					 "<span class='lotusBtn lotusLeft'>" +
+					 list[1].innerHTML +
+					 "</span>" +
+					 "<span class='lotusBtn lotusLeft'>" +
+					 list[2].innerHTML +
+					 "</span>" +
+					 "</div>", dojo.query(".inlinelist")[n],"replace");
+		
 		}
 	
   	}
