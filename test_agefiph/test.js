fcacbb8ba7647badf3b583d72eeaa6b3bb1c4d75
Vincent GICQUEL <37332811+vgicquel@users.noreply.github.com>
2018-09-10 12:27:55 +0200

    Update test.js

--- test_agefiph/test.js
+++ test_agefiph/test.js
@@ -8,12 +8,27 @@ let TestJS = new __cBill_waitForDojo('TestJS');
 TestJS.do(function () { 
   
 		/* Recherche de la balise */    
-		console.debug(dojo.query(".inlinelist"));
-		/*var n;*/
-	
 		console.debug(dojo.query("div[class='' role='navigation' style='font-size: 0.9em;' aria-label=Page navigation']"));
-	
 		console.debug(dojo.query("ul[class='inlinelist lotusInlinelist']"));
+	
+		var list = document.getElementsByClassName("inlinelist lotusInlinelist")[0].getElementsByTagName("LI");
+		for (i=0; i < list.length; i++) {
+		//alert(list[i].innerHTML);
+		}
+		
+		for (n=0; n <= dojo.query("ul[class='inlinelist lotusInlinelist']").length; n++) {
+			  dojo.place("<div class='lotusActionBar lotusBtnContainer aria_toolbar' role='toolbar' style='float:none' aria-label='Actions de blogue'>" + 
+				 "<span class='lotusBtn lotusLeft'>" +
+				 list[0].innerHTML + 
+				 "</span>" +
+				 "<span class='lotusBtn lotusLeft'>" +
+				 list[1].innerHTML +
+				 "</span>" +
+				 "<span class='lotusBtn lotusLeft'>" +
+				 list[2].innerHTML +
+				 "</span>" +
+				 "</div>", dojo.query(".inlinelist")[n],"replace");
+		}
 	
   	}
 	  
