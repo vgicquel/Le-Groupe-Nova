7d34142a1256205d782c1a983048f96045dceebc
Vincent GICQUEL <37332811+vgicquel@users.noreply.github.com>
2019-04-18 15:29:53 +0200

    Create search.js

---
+++ search_agefiph/search.js
@@ -0,0 +1,12 @@
+let SearchJS = new __cBill_waitForDojo('SearchJS'); 
+SearchJS.do(function () { 
+	
+		/* Recherche de la balise */ 
+		console.debug(dojo.query("div[style='border-top: 2px solid black;height:45px;']"));
+
+		/* Suppression de la ligne noire */
+		dojo.query("div[style='border-top: 2px solid black;height:45px;']").style("border-top","0px solid black")
+	
+	}
+	  
+)
