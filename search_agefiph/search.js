77c15669eb049b086cfcff00935bb9b7383e4790
Vincent GICQUEL <37332811+vgicquel@users.noreply.github.com>
2019-04-18 23:09:17 +0200

    Update search.js

--- search_agefiph/search.js
+++ search_agefiph/search.js
@@ -2,7 +2,7 @@ let SearchJS = new __cBill_waitForDojo('SearchJS');
 SearchJS.do(function () { 
 	
 		/* Recherche de la balise */ 
-		console.debug(dojo.query("<a role='button' href='javascript:setAllChecked('filterComponents',true)'>Select All</a>"));
+		console.debug(dojo.query("<a role="button" href="javascript:setAllChecked('filterComponents',true)">"));
 
 		/* Suppression de la ligne noire */
 		/*dojo.query("div[style='border-top: 2px solid black;height:45px;']").style("border-top","0px solid black")*/
