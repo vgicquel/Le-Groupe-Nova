ff4c60e0096e6295315f0581d2cd08c8ca0149c8
Vincent GICQUEL <37332811+vgicquel@users.noreply.github.com>
2019-04-23 11:04:31 +0200

    Update search.js

--- search_agefiph/search.js
+++ search_agefiph/search.js
@@ -2,7 +2,9 @@ let SearchJS = new __cBill_waitForDojo('SearchJS');
 SearchJS.do(function () { 
 	
 		/* Recherche de la balise */ 
-		console.debug(dojo.query("<a role='button' href='javascript:setAllChecked('filterComponents',true)'>"));
+		console.debug(dojo.query(".lotusInlinelist > li:nth-child(1) > a:nth-child(1)"));
+	
+	
 
 		/* Suppression de la ligne noire */
 		/*dojo.query("div[style='border-top: 2px solid black;height:45px;']").style("border-top","0px solid black")*/
